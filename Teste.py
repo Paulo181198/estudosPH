2675e55c97cf5e5c107b234cf198949ad5538aa9
Paulo181198 <paulosales.ph@gmail.com>
2021-01-26 13:44:33 -0300

    Adicionados métodos comer e falar.

--- Teste.py
+++ Teste.py
@@ -1,6 +1,9 @@
 from pessoa import Pessoa
 
-p1 = Pessoa()
 
-p1.falar("Carros")
+p1 = Pessoa("João", "18", "Masculino")
+
+p1.falar("carros")
+
+p1.comer("Banana")
 
--- pessoa.py
+++ pessoa.py
@@ -1,4 +1,14 @@
 class Pessoa:
     
+    def __init__ (self, nome, idade, sexo, fala=True, come=True, anda=True):
+
+        self.nome = nome
+        self.idade = idade
+        self.sexo = sexo
+
+
     def falar(self, assunto):
-        print("A pessoa está falando sobre", assunto)
+        print(f'O/A {self.nome} está falando sobre {assunto}.')
+    
+    def comer(self, alimento):
+        print(f'O/A {self.nome} está comendo {alimento}.')
