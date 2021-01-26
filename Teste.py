f97c7fae9cdc9674fa31343815a68852ca0c6469
Paulo181198 <paulosales.ph@gmail.com>
2021-01-26 14:36:30 -0300

    Adicionadas features de relação entre comer e falar

--- Teste.py
+++ Teste.py
@@ -3,7 +3,8 @@ from pessoa import Pessoa
 
 p1 = Pessoa("João", "18", "Masculino")
 
-p1.falar("carros")
 
-p1.comer("Banana")
-
+p1.Falar("carros")
+p1.Comer("Banana")
+p1.PararFalar()
+p1.Comer("Banana")
